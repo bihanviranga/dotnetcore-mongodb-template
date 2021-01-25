722300d9f3a9a84dc6f334d93ed4973bd46ba04a
Bihan Viranga <bihanviranga@gmail.com>
2021-01-25 15:18:25 +0530

    Added Student and Course services

---
+++ Services/StudentService.cs
@@ -0,0 +1,46 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using dotnetcore_mongodb_template.Interfaces;
+using dotnetcore_mongodb_template.Models;
+using MongoDB.Driver;
+
+namespace dotnetcore_mongodb_template.Services
+{
+    public class StudentService
+    {
+        private readonly IMongoCollection<Student> _students;
+
+        public StudentService(ISchoolDatabaseSettings settings)
+        {
+            var client = new MongoClient(settings.ConnectionString);
+            var database = client.GetDatabase(settings.DatabaseName);
+            _students = database.GetCollection<Student>(settings.StudentsCollectionName);
+        }
+
+        public async Task<List<Student>> GetAllAsync()
+        {
+            return await _students.Find(s => true).ToListAsync();
+        }
+
+        public async Task<Student> GetByIdAsync(string id)
+        {
+            return await _students.Find<Student>(s => s.Id == id).FirstOrDefaultAsync();
+        }
+
+        public async Task<Student> CreateAsync(Student student)
+        {
+            await _students.InsertOneAsync(student);
+            return student;
+        }
+
+        public async Task UpdateAsync(string id, Student student)
+        {
+            await _students.ReplaceOneAsync(s => s.Id == id, student);
+        }
+
+        public async Task DeleteAsync(string id)
+        {
+            await _students.DeleteOneAsync(s => s.Id == id);
+        }
+    }
+}
